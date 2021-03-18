cf1e982a74ec0560d99a90dec3c08d5e8bb6bac8
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-03-19 01:21:41 +0600

    another commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,38 @@ import Body from './components/Body/Body';
 import Destination from './components/Destination/Destination';
 import Header from './components/Header/Header';
 import Login from './components/LogIn/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className='App'>
-      <Header></Header>
-      {/* <Body></Body> */}
-      {/* <Login></Login> */}
-      <Destination></Destination>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/body">
+            <Body></Body>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/destination">
+            <Destination></Destination>
+          </Route>
+          <Route path="/">
+            <Body></Body>
+          </Route>
+        </Switch>
+      </Router>
+
+
+
+
+
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='navbar'>
             <h3>Urban Ride</h3>
             <ul>
-                <li>Home</li>
-                <li>Destination</li>
-                <li>Blog</li>
-                <li>Contact</li>
-                <li><button className='button'>Log in</button></li>
+                <li><Link to="/" style={{ textDecoration: 'none' }}>Home</Link></li>
+                <li><Link to="/destination" style={{ textDecoration: 'none' }}>Destination</Link></li>
+                <li><Link to="/blog" style={{ textDecoration: 'none' }}>Blog</Link></li>
+                <li><Link to="/contact" style={{ textDecoration: 'none' }}>Contact</Link></li>
+                <li><Link to="/login"><button className='button'>Log in</button></Link></li>
             </ul>
         </div>
     );
